970dc95b8c8048171c868f421d83101a5b8cc3e7
Sérgio Lopes <sergio.lopes@caelum.com.br>
2016-01-05 14:29:10 -0200

    Lista estatica simples de bolos na Home

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,2 +1,9 @@
 /* controllers da PedidaApp */
 /* PS. Em uma App complexa, crie arquivos separados para cada controller */
+
+/* controllers da PedidaApp */
+/* PS. Em uma App complexa, crie arquivos separados para cada controller */
+
+angular.module('starter').controller('HomeController', function($scope) {
+
+});
--- www/js/routes.js
+++ www/js/routes.js
@@ -1,1 +1,14 @@
 /* rotas da PedidaApp */
+
+angular.module('starter').config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider
+
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html',
+    controller: 'HomeController'
+  })
+
+});
