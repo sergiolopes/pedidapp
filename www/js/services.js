84f515acacb8cc45910a9ba8a09409aafc72affa
Sérgio Lopes <sergio.lopes@caelum.com.br>
2016-01-06 21:39:19 -0200

    service offline-first

--- www/js/services.js
+++ www/js/services.js
@@ -3,12 +3,24 @@
 
 angular.module('starter').service('ProdutosService', function($http, $q) {
 
+	// sempre dispara o serviço pra checar dados mais recentes
+	var promise = $http.get('http://cozinhapp.sergiolopes.org/produtos?random=1')
+			.then(function(response){
+				localStorage.setItem('cache', JSON.stringify(response.data));
+				return response.data;
+			});
+
+	// procura no localStorage
+	var cache = localStorage.getItem('cache');
+	if (cache != null) {
+		promise = $q(function(resolve, reject) {
+			resolve(JSON.parse(cache));
+		});
+	}
+
 	return {
-		lista: function(){
-			return $http.get("http://cozinhapp.sergiolopes.org/produtos")
-						.then(function(response){
-							return response.data;
-						});
+		lista: function() {
+			return promise;
 		}
 	};
 
