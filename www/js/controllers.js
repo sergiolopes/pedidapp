03ecc805f71dac33242e0ce72d7038a78239f697
Sérgio Lopes <sergio.lopes@caelum.com.br>
2016-01-06 11:47:33 -0200

    ajax

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,8 @@
 /* controllers da PedidaApp */
 /* PS. Em uma App complexa, crie arquivos separados para cada controller */
 
-angular.module('starter').controller('HomeController', function($scope) {
-
-    $scope.bolos = [
-        { nome: "Só de Cenoura", preco: 18 },
-        { nome: "Com Nutella", preco: 29 },
-        { nome: "De Brigadeiro", preco: 24 },
-        { nome: "Açucarado", preco: 19 }
-      ];
-
+angular.module('starter').controller('HomeController', function($scope, ProdutosService) {
+	ProdutosService.lista().then(function(dados){
+		$scope.bolos = dados;	
+	});
 });
-
--- www/js/services.js
+++ www/js/services.js
@@ -1,2 +1,15 @@
 /* services da PedidaApp */
 /* PS. Em uma App complexa, crie arquivos separados para cada service */
+
+angular.module('starter').service('ProdutosService', function($http, $q) {
+
+	return {
+		lista: function(){
+			return $http.get("http://cozinhapp.sergiolopes.org/produtos")
+						.then(function(response){
+							return response.data;
+						});
+		}
+	};
+
+});
