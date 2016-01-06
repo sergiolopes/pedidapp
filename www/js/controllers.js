3bbf7fb0aa1d95fd34a0922302f7fcb680c172cb
Sérgio Lopes <sergio.lopes@caelum.com.br>
2016-01-06 11:46:39 -0200

    dados dinamicos fixos

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,9 +1,14 @@
 /* controllers da PedidaApp */
 /* PS. Em uma App complexa, crie arquivos separados para cada controller */
 
-/* controllers da PedidaApp */
-/* PS. Em uma App complexa, crie arquivos separados para cada controller */
-
 angular.module('starter').controller('HomeController', function($scope) {
 
+    $scope.bolos = [
+        { nome: "Só de Cenoura", preco: 18 },
+        { nome: "Com Nutella", preco: 29 },
+        { nome: "De Brigadeiro", preco: 24 },
+        { nome: "Açucarado", preco: 19 }
+      ];
+
 });
+
