acad56b2953ad1127edccd53c4e6e77b22419cdc
Sérgio Lopes <sergio.lopes@caelum.com.br>
2016-01-06 15:06:14 -0200

    Pedido completo

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,7 +15,7 @@ angular.module('starter').controller('DetalheController', function($scope, Produ
 });
 
 
-angular.module('starter').controller('PedidoController', function($scope, $stateParams, $state, $http, $ionicPopup, $ionicLoading, ProdutosService) {
+angular.module('starter').controller('PedidoController', function($scope, $stateParams, $http, $state, $ionicPopup, $ionicLoading, ProdutosService) {
 
 	ProdutosService.lista().then(function(dados){
 		$scope.bolo = dados[$stateParams.boloId];
@@ -24,13 +24,40 @@ angular.module('starter').controller('PedidoController', function($scope, $state
 	$scope.dados = {};
 
 	$scope.fecharPedido = function() {
-	    $http.get('http://cozinhapp.sergiolopes.org/novo-pedido', {
-	        params: {
-	            pedido: $scope.bolo.nome,
-	            info: $scope.dados.nome 
-	                  + ' (' + $scope.dados.telefone + ') - ' 
-	                  + $scope.dados.endereco
-	        }
-	    });
+
+		// mostra o spinner de loading
+		$ionicLoading.show();
+
+		// dispara a API
+		$http.get('http://cozinhapp.sergiolopes.org/novo-pedido', {
+			params: {
+				pedido: $scope.bolo.nome,
+				info: $scope.dados.nome 
+				      + ' (' + $scope.dados.telefone + ') - ' 
+				      + $scope.dados.endereco
+			}
+		}).then(function() {
+
+			// caso OK, mostra popup confirmando e
+			// então navega pra home
+			$ionicPopup.alert({
+				title: 'Pedido confirmado!',
+				template: 'Daqui a pouco chega :)'
+			}).then(function(){
+				$state.go('home');
+			});
+
+		}).catch(function(erro) {
+
+			// caso dê erro mostra alerta com o erro
+			$ionicPopup.alert({
+				title: 'Erro no pedido!',
+				template: erro.data + '. Liga pra gente: 011-1406'
+			});
+
+		}).finally(function(){
+			// em qualquer caso, remove o spinner de loading
+			$ionicLoading.hide();
+		});
 	};
 });
